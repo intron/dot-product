1fd4806dfbdef3af37538825b2ebf16970dc4bb4
intron <alex.alekseyenko@gmail.com>
2014-02-23 19:25:47 -0800

    add comment to test.js with expected result (32)

--- test.js
+++ test.js
@@ -6,6 +6,6 @@ describe('dot-product', function(){
   it("Should compute the dot product of two arrays", function(){
     var a = [1, 2, 3];
     var b = [4, 5, 6];
-    assert.equal(32, dot(a, b)); 
+    assert.equal(32, dot(a, b)); // 32 
   });
 });
